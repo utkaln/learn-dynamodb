f0283169064c9dec5b33ec8b43d3bf670011704e
Utkal Nayak <nayak.utkal@gmail.com>
2022-05-27 22:32:19 -0400

    Create basic-dynamo-cli.sh

---
+++ basic-dynamo-cli.sh
@@ -0,0 +1,32 @@
+# Start dynamo db local
+java -Djava.library.path=./DynamoDBLocal_lib -jar DynamoDBLocal.jar -sharedDb
+
+# List tables from dynamodb in local
+aws dynamodb list-tables --endpoint-url http://localhost:8000
+
+# Create a table with primary key is a composite of partition key `Artist` and sort key `SongTitle`
+aws dynamodb create-table \
+    --table-name Music \
+    --attribute-definitions \
+        AttributeName=Artist,AttributeType=S \
+        AttributeName=SongTitle,AttributeType=S \
+    --key-schema AttributeName=Artist,KeyType=HASH AttributeName=SongTitle,KeyType=RANGE \
+    --provisioned-throughput ReadCapacityUnits=1,WriteCapacityUnits=1 \
+    --table-class STANDARD
+    
+# Put Item into the table
+aws dynamodb put-item \
+    --table-name Music \
+    --item \
+        '{"Artist": {"S": "No One You Know"}, "SongTitle": {"S": "Call Me Today"}, "AlbumTitle": {"S": "Somewhat Famous"}}' \
+    --return-consumed-capacity TOTAL  
+
+aws dynamodb put-item \
+    --table-name Music \
+    --item '{ \
+        "Artist": {"S": "Acme Band"}, \
+        "SongTitle": {"S": "Happy Day"}, \
+        "AlbumTitle": {"S": "Songs About Life"} }' \
+    --return-consumed-capacity TOTAL
+    
+    
